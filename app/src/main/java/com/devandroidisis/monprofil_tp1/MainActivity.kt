3bf7b514f6ea11cb2b2aeb727beaae3e41def95f
Damien Lopez <damien-lopez@etud.univ-jfc.fr>
2023-09-13 10:17:27 +0200

    Fin de la premiere scéance

--- app/src/main/java/com/devandroidisis/monprofil_tp1/MainActivity.kt
+++ app/src/main/java/com/devandroidisis/monprofil_tp1/MainActivity.kt
@@ -3,12 +3,22 @@ package com.devandroidisis.monprofil_tp1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.devandroidisis.monprofil_tp1.ui.theme.MonProfil_TP1Theme
 
@@ -19,7 +29,7 @@ class MainActivity : ComponentActivity() {
             MonProfil_TP1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -27,17 +37,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Box(contentAlignment = Alignment.Center , modifier = modifier.fillMaxSize()) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.photomonprofil),
+                contentDescription = "My profile picture",
+                Modifier.clip(CircleShape)
+            )
+            Text(
+                text = "Damien Lopez",
+                fontWeight = FontWeight.Bold,
+                modifier = modifier
+            )
+            Text(
+                text = "Etudiant FIE4 - ISIS",
+                modifier = modifier
+            )
+            Text(
+                text = "Ecole d'ingénieurs ISIS - INU Champollion'",
+                fontStyle = FontStyle.Italic,
+                modifier = modifier
+            )
+
+        }
+        
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MonProfil_TP1Theme {
-        Greeting("Android")
+    Greeting()
     }
 }
